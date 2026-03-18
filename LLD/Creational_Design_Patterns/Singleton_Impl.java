b5b23f339971766eabbc05f2189a1a8b92d08470
Nikhil Sharma <53441733+Nikhilsharma-9099@users.noreply.github.com>
2026-03-18 20:35:45 +0530

    Update Singleton_Impl.java

--- LLD/Creational_Design_Patterns/Singleton_Impl.java
+++ LLD/Creational_Design_Patterns/Singleton_Impl.java
@@ -1,1 +1,56 @@
+// Synchronized Method
+public class Singleton {
+    // Object declaration
+    private static Singleton instance;
+
+    // Private constructor
+    private Singleton() {}
+
+    // Synchronized keyword used
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+// Double-Checked Locking
+public class Singleton {
+    // Volatile object declaration
+    private static volatile Singleton instance;
+
+    // Private constructor
+    private Singleton() {}
+
+    // Thread-safe method using double-checked locking
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+//Bill Pugh Singleton (Best Practice for Lazy Loading)
+public class Singleton {
+    // Private constructor
+    private Singleton() {}
+
+    // Static inner class to hold the Singleton instance
+    private static class Holder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    // Public method to return the Singleton instance
+    public static Singleton getInstance() {
+        return Holder.INSTANCE;
+    }
+}
+
+
 
